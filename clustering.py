23927931fb83b60160b9610e20842d67a1735de3
ali kasiri <akgh1382@gmail.com>
2024-06-27 22:29:45 +0330

    cleaning the code

--- clustering.py
+++ clustering.py
@@ -1,59 +1,12 @@
-from PIL import Image
-import os
 import numpy as np 
 import pandas as pd 
-import torchvision.transforms as transforms
-from torchvision.models import resnet50
-import random
-import torch
-import matplotlib.pyplot as plt 
-import cv2
-from scipy.signal import savgol_filter
-from skimage.feature import hog
-from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
-import seaborn as sns
-from skimage import  exposure
-from sklearn.cluster import OPTICS
-from sklearn.cluster import DBSCAN
+from sklearn.metrics import silhouette_score 
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import SpectralClustering
 from scipy.spatial.distance import pdist, cdist
 
-
-
-def extract_number(folder_name):
-    return int(folder_name.split('.')[0])
-
-def extract_number_files(folder_name):
-    return int(folder_name.split('EX')[1].split('.')[0])
-
-def get_images_path_list(input_folder) :
-    path_list = []
-    parent_folder_path = input_folder
-    folders = [f for f in os.listdir(parent_folder_path) if os.path.isdir(os.path.join(parent_folder_path, f))]
-    sorted_folders = sorted(folders, key=extract_number)
-
-    for dirs in sorted_folders:
-        files = os.listdir(parent_folder_path+dirs)
-        file_names = [f for f in files if os.path.isfile(os.path.join(parent_folder_path+dirs, f))]
-        file_names = sorted(file_names, key=extract_number_files)
-        # print(dirs)
-        for filename in file_names:
-            image_path = os.path.join(parent_folder_path+dirs, filename)
-            # print(image_path)
-            path_list.append(image_path)
-
-    return path_list
-
-
-def apply_pca(data,name, n_components=2):
-
-    pca = PCA(n_components=n_components)
-    principal_components = pca.fit_transform(data)
-
-    pca_columns = [f'PC_{name}_{i+1}' for i in range(principal_components.shape[1])] # for i in range(n_components)
-
-    return pd.DataFrame(data=principal_components, columns=pca_columns)
 
 def calculate_dunn_index(data, labels):
     clusters = np.unique(labels)
@@ -68,6 +21,7 @@ def calculate_dunn_index(data, labels):
             intra_cluster_distance = 0
         intra_cluster_distances.append(intra_cluster_distance)
     
+    
     for i in range(len(clusters)):
         for j in range(i + 1, len(clusters)):
             cluster_i_points = data[labels == clusters[i]]
@@ -77,166 +31,109 @@ def calculate_dunn_index(data, labels):
     
     dunn_index = np.min(inter_cluster_distances) / np.max(intra_cluster_distances)
     return dunn_index
-
-def plot_hog_example(image):
-    fd, hog_image = hog(image, orientations=8 , pixels_per_cell=(16, 16) ,cells_per_block=(1, 1),
-                    visualize=True, channel_axis=-1)
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
-
-    ax1.axis('off')
-    ax1.imshow(image)
-    ax1.set_title('Input image')
-
-    # Rescale histogram for better display
-    hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
-
-    ax2.axis('off')
-    ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray)
-    ax2.set_title('Histogram of Oriented Gradients')
-    plt.show()
-
-
-def plot_five_ex_clusters(clusters , images):
-
-    # 5. Filter out noise and small clusters
-    unique_clusters = np.unique(clusters)
-    filtered_clusters = [cluster for cluster in unique_clusters if cluster != -1]
-
-    # 6. Select representative images from each cluster
-    n_examples = 5
-    example_images = {cluster: [] for cluster in filtered_clusters}
-
-    for cluster in filtered_clusters:
-        cluster_indices = np.where(clusters == cluster)[0]
-        if len(cluster_indices) > 4:  # Minimum cluster size threshold
-            selected_indices = random.sample(list(cluster_indices), min(n_examples, len(cluster_indices)))
-            example_images[cluster] = selected_indices
-
-    # 7. Plot example images from each cluster
-    plt.figure(figsize=(15, len(example_images) * 3))
-    for i, (cluster, indices) in enumerate(example_images.items()):
-        for j, index in enumerate(indices):
-            plt.subplot(len(example_images), n_examples, i * n_examples + j + 1)
-            plt.imshow(images[index], cmap=plt.cm.gray)
-            plt.axis('off')
-            if j == 0:
-                plt.title(f'Cluster {cluster}')
-    plt.tight_layout()
-    plt.show()
-
-def Calculate_avg_leaf_to_background_ratio(binary_images):
-
-    black_pixels = np.sum(binary_images == 0)
-    white_pixels = np.sum(binary_images == 255)
-    average_leaf_to_background_ratio = white_pixels /  black_pixels if black_pixels != 0 else float('nan')
-    return average_leaf_to_background_ratio
-
-def find_Length_and_width(image_array):
-    y_start = 0
-    x_start = 0
-    y_end , x_end = image_array.shape
-    
-    # find y_start , y_end
-    for i in range(image_array.shape[0]): 
-        if (image_array[i].sum() > 0) and (y_start == 0):
-            y_start = i
-
-        if (image_array[-i].sum()>0) and (y_end == image_array.shape[0]):
-            y_end = image_array.shape[0] - i
-    # find x_start , x_end
-    for j in range(image_array.shape[1]): 
-        if (image_array[: , j].sum() > 0) and (x_start == 0):
-            x_start = j
-        if (image_array[: , -j].sum() > 0) and (x_end == image_array.shape[1]):
-            x_end = image_array.shape[1] - j
-
-    return y_start , y_end , x_start , x_end
-
     
 
+    
+    
+
+class Clustering():
+    def __init__(self,model='spectral') -> None:
+        self.model = model
+        self.inf_list = []
+        self.clusters = []
+        self.k = 0
+    def find_best_k_cluster(self , data , k_range = (2,50) ):
+        dunn_list = []
+        silhouette_list = []
         
-class Images_class():
-    def __init__(self) -> None:
-        self.cnn_model = resnet50(pretrained=True)
-        self.rgb_images = []
-        self.gray_images = []
-        self.binary_images = []
-        self.contours_images=[]
-        self.list_distances=[]
-        self.pil_images = []
-        self.cnn_feature = np.array([])
-        self.list_images_hog= np.array([])
+        for i in range(k_range[0] , k_range[1]):
+            dic = {}
 
-    def get_images_inf(self , input_folder ):
-        path_list = get_images_path_list(input_folder )
-        for image_path in path_list:
-                
-            image = cv2.imread(image_path)
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            if self.model == 'spectral':
+                self.clustering_model = SpectralClustering(n_clusters=i, random_state=42 )
 
-            blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-            _, thresholded = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+            elif self.model  == 'AgglomerativeClustering':
+                self.clustering_model = AgglomerativeClustering(n_clusters=i)
+
+            elif self.model  == 'kmeans':
+                self.clustering_model = KMeans(n_clusters=i, random_state=42)
+
             
-            contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            clusters = self.clustering_model.fit_predict(data)
 
-            cnt = tuple()
-            for i in range(len(contours)):
-                if len(cnt) < len(contours[i]):
-                    cnt = contours[i]
+            dunn_avg = calculate_dunn_index(data, clusters)
+            silhouette_avg = silhouette_score(data, clusters)
+            dic['silhouette_avg'] = silhouette_avg
+            dic['dunn_avg'] = dunn_avg
+            dic['k'] = i
+            dic['model'] = self.model
+            dunn_list.append(dunn_avg)
+            silhouette_list.append(silhouette_avg)
+            self.inf_list.append(dic)
+            
+        print(f'finding best k cluster with model {self.model} is done')
 
-            img = Image.open(image_path).convert('RGB')
-            # img = img.resize(target_size)
+        self.inf_list = sorted(self.inf_list, key=lambda x: (-x['silhouette_avg']))
+        self.inf_df = pd.DataFrame(self.inf_list)
 
-            self.pil_images.append(img)
-            self.rgb_images.append(image)
-            self.gray_images.append(gray)
-            self.binary_images.append(thresholded)
-            self.contours_images.append(cnt)
 
-    def extract_cnn_features(self, target_size:tuple=(192,144)):
+    def plot_best_K(self , n_clusters=5):
+        list_silhouette = []
+        for i in sorted(self.inf_list, key=lambda x: (x['k'])):
+            list_silhouette.append(i['silhouette_avg'])
 
-        model = self.cnn_model.eval()  # Set to evaluation mode
-        # Define the transformation for preprocessing images
-        preprocess = transforms.Compose([
-            transforms.Resize(target_size),
-            transforms.ToTensor(),
-        ])
+        plt.plot(range(2,50),list_silhouette, marker='o', linestyle='-', color='b', label='silhouettes')
+        max_value = max(list_silhouette)
+        max_index = list_silhouette.index(max_value)
+        plt.axhline(y=max_value, color='r', linestyle='-', label=f'Maximum silhouette: {max_value}')
+        plt.axvline(x=max_index+2, color='r', linestyle='--', label=f'K: {max_index+2}')
 
+        plt.xlabel('K')
+        plt.ylabel('silhouette')
+        plt.title(f'Plot silhouette score in deferent K ({self.model})')
+        plt.legend()
+        plt.show()
+        plt.close()
+
+
+        cluster_df = pd.DataFrame(self.inf_list[ : n_clusters])
+        bar_width = 0.35
+        r1 = np.arange(len(cluster_df['k']))
+        r2 = [x + bar_width for x in r1]
+
+        fig, ax = plt.subplots()
+        bars1 = ax.bar(r1, cluster_df['dunn_avg'], color='blue', width=bar_width, edgecolor='grey', label='Dunn Score')
+        bars2 = ax.bar(r2, cluster_df['silhouette_avg'], color='green', width=bar_width, edgecolor='grey', label='Silhouette Score')
+
+
+        ax.set_xlabel('k', fontweight='bold')
+        ax.set_ylabel('Scores', fontweight='bold')
+        ax.set_title(f' Dunn  and Silhouette vs k ({self.model})')
+        ax.set_xticks([r + bar_width / 2 for r in range(len(cluster_df['k']))])
+        ax.set_xticklabels(cluster_df['k'])
+
+        ax.legend()
+        plt.show()
+
+    def fit_predict(self , k ,data):
+        if self.model == 'spectral':
+                self.clustering_model = SpectralClustering(n_clusters=k, random_state=42 )
+
+        elif self.model  == 'AgglomerativeClustering':
+            self.clustering_model = AgglomerativeClustering(n_clusters=k)
+
+        elif self.model  == 'kmeans':
+            self.clustering_model = KMeans(n_clusters=k, random_state=42)
+
+        self.clusters = self.clustering_model.fit_predict(data)
+        self.k = k
         
-        features = []
-        for img in self.pil_images:
-            img_tensor = preprocess(img).unsqueeze(0)
-            with torch.no_grad():
-                feature = model(img_tensor).squeeze().numpy()
-            features.append(feature)
+    def plot_data_with_clusters(self , data):
 
-
-        self.cnn_feature = np.array(features)
-
-    def Calculate_distance_each_contour_from_centroid(self):
-        for cnt in self.contours_images:
-            M = cv2.moments(cnt)
-            centroid_x = int(M['m10'] / M['m00'])
-            centroid_y = int(M['m01'] / M['m00'])
-
-            # Calculate distance of each contour point from the centroid
-            distances = []
-            for point in cnt:
-                x, y = point[0]
-                distance = np.sqrt((x - centroid_x) ** 2 + (y - centroid_y) ** 2)
-                distances.append(distance)
-
-            # Convert to a numpy array and normalize
-            distances = np.array(distances)
-            distances = distances / np.max(distances)
-
-            self.list_distances.append(list(distances))
-
-    
-    def get_pictures_hog(self):
-        hog_list = []
-        for image in self.rgb_images:
-            fd , hog_image = hog(image, orientations=8 , pixels_per_cell=(16, 16) ,cells_per_block=(1, 1),visualize=True, channel_axis=-1)
-            hog_list.append(fd)
-
-        self.list_images_hog = np.vstack(hog_list)
+        plt.figure(figsize=(10, 6))
+        scatter = plt.scatter(data.iloc[:, 0], data.iloc[:, 1], c=self.clusters, cmap='viridis', alpha=0.5)
+        plt.colorbar(scatter)
+        plt.xlabel('Principal Component 1')
+        plt.ylabel('Principal Component 2')
+        plt.title(f'{self.model} Clustering of Leaves (k={self.k})')
+        plt.show()
---
+++ fiture_extraction.py
@@ -0,0 +1,250 @@
+from PIL import Image
+import os
+import numpy as np 
+import pandas as pd 
+import torchvision.transforms as transforms
+from torchvision.models import resnet50
+import random
+import torch
+import matplotlib.pyplot as plt 
+import cv2
+from skimage.feature import hog
+from sklearn.decomposition import PCA
+from skimage import  exposure
+from scipy.spatial.distance import pdist, cdist
+
+
+
+def extract_number(folder_name):
+    return int(folder_name.split('.')[0])
+
+def extract_number_files(folder_name):
+    return int(folder_name.split('EX')[1].split('.')[0])
+
+def get_images_path_list(input_folder) :
+    path_list = []
+    parent_folder_path = input_folder
+    folders = [f for f in os.listdir(parent_folder_path) if os.path.isdir(os.path.join(parent_folder_path, f))]
+    sorted_folders = sorted(folders, key=extract_number)
+
+    for dirs in sorted_folders:
+        files = os.listdir(parent_folder_path+dirs)
+        file_names = [f for f in files if os.path.isfile(os.path.join(parent_folder_path+dirs, f))]
+        file_names = sorted(file_names, key=extract_number_files)
+        # print(dirs)
+        for filename in file_names:
+            image_path = os.path.join(parent_folder_path+dirs, filename)
+            # print(image_path)
+            path_list.append(image_path)
+
+    return path_list
+
+
+def apply_pca(data,name, n_components=2):
+
+    pca = PCA(n_components=n_components)
+    principal_components = pca.fit_transform(data)
+
+    pca_columns = [f'PC_{name}_{i+1}' for i in range(principal_components.shape[1])] # for i in range(n_components)
+
+    return pd.DataFrame(data=principal_components, columns=pca_columns)
+
+def calculate_dunn_index(data, labels):
+    clusters = np.unique(labels)
+    intra_cluster_distances = []
+    inter_cluster_distances = []
+    
+    for cluster in clusters:
+        cluster_points = data[labels == cluster]
+        if len(cluster_points) > 1:
+            intra_cluster_distance = np.max(pdist(cluster_points))
+        else:
+            intra_cluster_distance = 0
+        intra_cluster_distances.append(intra_cluster_distance)
+    
+    for i in range(len(clusters)):
+        for j in range(i + 1, len(clusters)):
+            cluster_i_points = data[labels == clusters[i]]
+            cluster_j_points = data[labels == clusters[j]]
+            inter_cluster_distance = np.min(cdist(cluster_i_points, cluster_j_points))
+            inter_cluster_distances.append(inter_cluster_distance)
+    
+    dunn_index = np.min(inter_cluster_distances) / np.max(intra_cluster_distances)
+    return dunn_index
+
+def plot_hog_example(image):
+    fd, hog_image = hog(image, orientations=8 , pixels_per_cell=(16, 16) ,cells_per_block=(1, 1),
+                    visualize=True, channel_axis=-1)
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
+
+    ax1.axis('off')
+    ax1.imshow(image)
+    ax1.set_title('Input image')
+
+    # Rescale histogram for better display
+    hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
+
+    ax2.axis('off')
+    ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray)
+    ax2.set_title('Histogram of Oriented Gradients')
+    plt.show()
+
+
+def plot_five_ex_clusters(clusters , images):
+
+    # 5. Filter out noise and small clusters
+    unique_clusters = np.unique(clusters)
+    filtered_clusters = [cluster for cluster in unique_clusters if cluster != -1]
+
+    # 6. Select representative images from each cluster
+    n_examples = 5
+    example_images = {cluster: [] for cluster in filtered_clusters}
+
+    for cluster in filtered_clusters:
+        cluster_indices = np.where(clusters == cluster)[0]
+        if len(cluster_indices) > 4:  # Minimum cluster size threshold
+            selected_indices = random.sample(list(cluster_indices), min(n_examples, len(cluster_indices)))
+            example_images[cluster] = selected_indices
+
+    # 7. Plot example images from each cluster
+    plt.figure(figsize=(15, len(example_images) * 3))
+    for i, (cluster, indices) in enumerate(example_images.items()):
+        for j, index in enumerate(indices):
+            plt.subplot(len(example_images), n_examples, i * n_examples + j + 1)
+            plt.imshow(images[index], cmap=plt.cm.gray)
+            plt.axis('off')
+            if j == 0:
+                plt.title(f'Cluster {cluster}')
+    plt.tight_layout()
+    plt.show()
+
+def Calculate_avg_leaf_to_background_ratio(binary_images):
+
+    black_pixels = np.sum(binary_images == 0)
+    white_pixels = np.sum(binary_images == 255)
+    average_leaf_to_background_ratio = white_pixels /  black_pixels if black_pixels != 0 else float('nan')
+    return average_leaf_to_background_ratio
+
+def find_Length_and_width(image_array):
+    y_start = 0
+    x_start = 0
+    y_end , x_end = image_array.shape
+    
+    # find y_start , y_end
+    for i in range(image_array.shape[0]): 
+        if (image_array[i].sum() > 0) and (y_start == 0):
+            y_start = i
+
+        if (image_array[-i].sum()>0) and (y_end == image_array.shape[0]):
+            y_end = image_array.shape[0] - i
+    # find x_start , x_end
+    for j in range(image_array.shape[1]): 
+        if (image_array[: , j].sum() > 0) and (x_start == 0):
+            x_start = j
+        if (image_array[: , -j].sum() > 0) and (x_end == image_array.shape[1]):
+            x_end = image_array.shape[1] - j
+
+    return y_start , y_end , x_start , x_end
+
+def plot_image_and_distances(binary_images , distances):
+    plt.figure(figsize=(12, 10))
+    plt.subplot(2, 2, 1)
+    plt.imshow(cv2.cvtColor(binary_images, cv2.COLOR_BGR2RGB))
+    # plt.plot([centroid_x], [centroid_y], marker='o', markersize=5, color="red")
+    plt.title("Leaf Image")
+
+
+    plt.subplot(2, 2, 2)
+    plt.plot(distances)
+    plt.title("Distances of Contour Points from Centroid")
+    plt.xlabel("Contour Point Index")
+    plt.ylabel("Distance from Centroid")
+
+    plt.show()
+
+        
+class Images_class():
+    def __init__(self) -> None:
+        self.cnn_model = resnet50(pretrained=True)
+        self.rgb_images = []
+        self.gray_images = []
+        self.binary_images = []
+        self.contours_images=[]
+        self.list_distances=[]
+        self.pil_images = []
+        self.cnn_feature = np.array([])
+        self.list_images_hog= np.array([])
+
+    def get_images_inf(self , input_folder ):
+        path_list = get_images_path_list(input_folder )
+        for image_path in path_list:
+                
+            image = cv2.imread(image_path)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+            _, thresholded = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+            
+            contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            cnt = tuple()
+            for i in range(len(contours)):
+                if len(cnt) < len(contours[i]):
+                    cnt = contours[i]
+
+            img = Image.open(image_path).convert('RGB')
+            # img = img.resize(target_size)
+
+            self.pil_images.append(img)
+            self.rgb_images.append(image)
+            self.gray_images.append(gray)
+            self.binary_images.append(thresholded)
+            self.contours_images.append(cnt)
+
+    def extract_cnn_features(self, target_size:tuple=(192,144)):
+
+        model = self.cnn_model.eval()  # Set to evaluation mode
+        # Define the transformation for preprocessing images
+        preprocess = transforms.Compose([
+            transforms.Resize(target_size),
+            transforms.ToTensor(),
+        ])
+
+        
+        features = []
+        for img in self.pil_images:
+            img_tensor = preprocess(img).unsqueeze(0)
+            with torch.no_grad():
+                feature = model(img_tensor).squeeze().numpy()
+            features.append(feature)
+
+
+        self.cnn_feature = np.array(features)
+
+    def Calculate_distance_each_contour_from_centroid(self):
+        for cnt in self.contours_images:
+            M = cv2.moments(cnt)
+            centroid_x = int(M['m10'] / M['m00'])
+            centroid_y = int(M['m01'] / M['m00'])
+
+            # Calculate distance of each contour point from the centroid
+            distances = []
+            for point in cnt:
+                x, y = point[0]
+                distance = np.sqrt((x - centroid_x) ** 2 + (y - centroid_y) ** 2)
+                distances.append(distance)
+
+            # Convert to a numpy array and normalize
+            distances = np.array(distances)
+            distances = distances / np.max(distances)
+
+            self.list_distances.append(list(distances))
+
+    
+    def get_pictures_hog(self):
+        hog_list = []
+        for image in self.rgb_images:
+            fd , hog_image = hog(image, orientations=8 , pixels_per_cell=(16, 16) ,cells_per_block=(1, 1),visualize=True, channel_axis=-1)
+            hog_list.append(fd)
+
+        self.list_images_hog = np.vstack(hog_list)
